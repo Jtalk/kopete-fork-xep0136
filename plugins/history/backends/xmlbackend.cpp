7ce16f86974211f98c72b4a780ddbd04c37d3363
Roman Nazarenko <me@jtalk.me>
2014-08-09 19:13:37 +0600

    Adding separate XML backend.

---
+++ plugins/history/backends/xmlbackend.cpp
@@ -0,0 +1,22 @@
+/*
+	xmlbackend.cpp
+
+	Copyright (c) 2003-2004 by Olivier Goffart        <ogoffart@kde.org>
+
+	Kopete    (c) 2003-2004 by the Kopete developers  <kopete-devel@kde.org>
+
+	*************************************************************************
+	*                                                                       *
+	* This program is free software; you can redistribute it and/or modify  *
+	* it under the terms of the GNU General Public License as published by  *
+	* the Free Software Foundation; either version 2 of the License, or     *
+	* (at your option) any later version.                                   *
+	*                                                                       *
+	*************************************************************************
+*/
+
+#include "xmlbackend.h"
+
+XMLBackend::XMLBackend()
+{
+}
---
+++ plugins/history/backends/xmlbackend.h
@@ -0,0 +1,27 @@
+/*
+	xmlbackend.h
+
+	Copyright (c) 2003-2004 by Olivier Goffart        <ogoffart@kde.org>
+
+	Kopete    (c) 2003-2004 by the Kopete developers  <kopete-devel@kde.org>
+
+	*************************************************************************
+	*                                                                       *
+	* This program is free software; you can redistribute it and/or modify  *
+	* it under the terms of the GNU General Public License as published by  *
+	* the Free Software Foundation; either version 2 of the License, or     *
+	* (at your option) any later version.                                   *
+	*                                                                       *
+	*************************************************************************
+*/
+
+#ifndef XMLBACKEND_H
+#define XMLBACKEND_H
+
+class XMLBackend
+{
+public:
+	XMLBackend();
+};
+
+#endif // XMLBACKEND_H
